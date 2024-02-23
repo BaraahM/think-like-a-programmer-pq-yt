2a3f5c6318564cc6a51d10287512b7b60e94490b
BARAAH ALAWI AHMED MEKYASH <BaraahMekyash@gmail.com>
2024-02-23 11:51:09 +0800

    Update find-replace.cpp

--- find-replace.cpp
+++ find-replace.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main() {
 
-    string fact = "Fyodor is an amazing writer";
-    cout << fact << endl;
-    size_t pos = fact.find("amazing");
-    if (pos != string::npos) {
-        fact.replace(pos, 7, "spectacular");
+    string fact = "Fyodor is an amazing writer"; //initailizing string
+    cout << fact << endl; //printing it
+    size_t pos = fact.find("amazing"); // initailizing the word u wanna replace
+    if (pos != string::npos) { // finding the word u wanna replace
+        fact.replace(pos, 7, "spectacular"); //replacing the word u wanna replace
     }
-    cout << fact << endl;
+    cout << fact << endl; //printing the string again ;)
     return 0;
 }
